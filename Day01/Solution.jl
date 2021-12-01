658b3bc626cddc4879032873d702e9530a06f70f
jorasinghr <40352266+jorasinghr@users.noreply.github.com>
2021-12-01 22:37:38 +0100

    tried with julia, have local issues

--- Day01/Solution.jl
+++ Day01/Solution.jl
@@ -1,8 +1,8 @@
 #=
 Solution:
-- Julia version: 0.4.2
+- Julia version: 1.7.0
 - Author: Jora Singh Randhawa
 - Date: 2021-12-01
 =#
-
-println("bfdm")
+input = open("input.txt", "r")
+depths = parse.(Int, readlines(input_file))
