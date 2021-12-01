684670468396caa223eaa3169bb54bb3f446b18a
jorasinghr <40352266+jorasinghr@users.noreply.github.com>
2021-12-01 21:14:23 +0100

    Restructuring folders

--- Day01/Day01.py
+++ Day01/Day01.py
@@ -2,24 +2,22 @@
 with open('Day01.txt') as f:
     lines = [int(x) for x in f.read().split()]
 
-#task 1
+# task 1
 count = 0
 for ind, val in enumerate(lines):
-    if ind != 0 or ind != len(lines)-1:
-        if val > lines[ind-1]:
+    if ind != 0 or ind != len(lines) - 1:
+        if val > lines[ind - 1]:
             count += 1
 
 print('Task 1:', count)
 
-#task 2
+# task 2
 count = 0
 for ind, val in enumerate(lines):
-    if ind < len(lines)-3:
-        A = val + lines[ind+1] + lines[ind+2]
-        B = lines[ind+1] + lines[ind+2] + lines[ind+3]
+    if ind < len(lines) - 3:
+        A = val + lines[ind + 1] + lines[ind + 2]
+        B = lines[ind + 1] + lines[ind + 2] + lines[ind + 3]
         if B > A:
             count += 1
 
 print('Task 2:', count)
-
-
